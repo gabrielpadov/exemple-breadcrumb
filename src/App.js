f924034d1ed2d58cf152a30ec97a1f999ebc8537
Gabriel Padovani <gabrielpadov@gmail.com>
2019-10-10 16:45:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Router } from "@reach/router";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Breadcrumb from "./Breadcrumb";
+
+const items = [
+  { to: "/", label: "Home" },
+  { to: "/dashboard", label: "Dashboard" },
+  { to: "/contact", label: "Contact" },
+  { to: "/about", label: "About" },
+  { to: "/blog", label: "Blog" }
+];
+
+const App = () => (
+  <div className="app">
+    <Breadcrumb>
+      {items.map(({ to, label }) => (
+        <Link key={to} to={to}>
+          {label}
+        </Link>
+      ))}
+    </Breadcrumb>
+    <Router>
+      <Home path="/" />
+      <Dashboard path="/dashboard" />
+      <Contact path="/contact" />
+      <About path="/about" />
+      <Blog path="/blog" />
+    </Router>
+  </div>
+);
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+// src/pages/home.js
+import React from "react";
+
+const Home = ({ children }) => (
+  <div>
+    <h2>Home</h2>
+    <div>{children}</div>
+  </div>
+);
+
+export default Home;
